5c25f2bf1dc8827002db288a1ededdf343d42cb6
medhavimangal08 <medhavimangal43@gmail.com>
2025-02-19 10:14:56 +0530

    props concept

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Tooltip text="This is a tooltip" children="Hover over me"></Tooltip>
+      <Tooltip
+        text="This is another tooltip"
+        children="Hover over me to see second tooltip"
+      ></Tooltip>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Tooltip.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ReactDom from "react-dom";
+
+const Tooltip = ({ text, children }) => {
+  let [isHovered, setisHovered] = useState(false);
+  return (
+    <div>
+      {isHovered && <p className="tooltiptext">{text}</p>}
+      <h1
+        className="tooltip"
+        onMouseEnter={() => setisHovered(true)}
+        onMouseLeave={() => setisHovered(false)}
+      >
+        {children}
+      </h1>
+    </div>
+  );
+};
+export default Tooltip;
